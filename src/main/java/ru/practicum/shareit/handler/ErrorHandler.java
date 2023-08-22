6b1e87bf712cf3b16b305cb2cf35cb747237d74d
hellotema90 <as.masalkin@yandex.ru>
2023-08-22 23:05:50 +0300

    1я проверка(1)

--- src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -41,6 +41,7 @@ public class ErrorHandler {
                 e.getMessage()
         );
     }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handlerAccessException(final AccessException e) {
